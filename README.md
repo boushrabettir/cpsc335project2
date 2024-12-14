16fb73ff94f885004e8d0a56bf74d1a41ec48f1e
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2024-12-13 23:24:39 -0800

    Pushing Code

---
+++ README.md
@@ -0,0 +1,26 @@
+## Project 3 - Path Finding Algorihtms
+
+**Team Member:**
+
+- Boushra Bettir (boushra.bettir04@csu.fullerton.edu)
+
+## Project Details
+
+We have two algorithms for pathfinding. The two are **Exhaustive Search** and **Dynamic Programming**.
+
+## Files
+
+- `main.cpp` has all of the code that will run both of the algorithms.
+- `README.md` has the explanation to understand the code and to run it.
+- `execution.txt` is for saving the scatter plots.
+
+## Running this Project
+
+### ! Before You Continue, please ensure you have a C++ compiler installed to your system. Without it you WILL NOT be able to run the project !
+
+To run the code,
+
+1. Clone the Repo using `git clone [LINK_TO_THIS_REPO]`
+2. Do `cd [LINK_TO_CLONED_REPO]`
+3. Compile it by doing `g++ -o main main.cpp` (If that does not work run it by doing `g++ -std=c++11 -o main main.cpp`).
+4. Run the code by doing `./main`
--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
+// Project 3 - Path Finding Algorithms
+// Team Members: Boushra Bettir (boushra.bettir04@csu.fullerton.edu)
+
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <chrono>
+#include <fstream> // To write to file
 
 using namespace std;
+using namespace std::chrono;
 
-// The following code will be the Exhaustive
-// Search algorithm and the other algorithmic approach
-// that we as a team created.
-
-// Algorithm Number 1
-int exhaustiveSearchAlgorithm(
-    const vector<vector<char>> &grid)
+// Algorithm Number 1 - Exhaustive Search
+int exhaustiveSearchAlgorithm(const vector<vector<char>> &grid)
 {
-
     char X = 'X';
     char Y = 'Y';
     int gridRow = grid.size();
     int gridColumn = grid[0].size();
     int moves = gridRow + gridColumn - 2;
 
+    // Final Counter
     int c = 0;
 
-    // i are the bits in the algorithm.
     for (int i = 0; i < (1 << moves); ++i)
     {
         int m = 0; // Down, Up
@@ -33,7 +33,6 @@ int exhaustiveSearchAlgorithm(
             if ((i >> n) & 1)
             {
                 m++;
-
                 if (m >= gridRow || grid[m][n] == X)
                 {
                     isValid = false;
@@ -42,9 +41,7 @@ int exhaustiveSearchAlgorithm(
             }
             else
             {
-
                 n++;
-
                 if (n >= gridColumn || grid[m][n] == Y)
                 {
                     isValid = false;
@@ -62,13 +59,11 @@ int exhaustiveSearchAlgorithm(
     return c;
 }
 
-// Algorithm Number 2
-int dynamicProgrammingAlgorithm(
-    const vector<vector<char>> &grid)
+// Algorithm Number 2 - Dynamic Programming
+int dynamicProgrammingAlgorithm(const vector<vector<char>> &grid)
 {
     int gridRow = grid.size();
     int gridColumn = grid[0].size();
-
     char X = 'X';
     char Y = 'Y';
 
@@ -78,9 +73,6 @@ int dynamicProgrammingAlgorithm(
     }
 
     vector<vector<int>> dynamicProgramming(gridRow, vector<int>(gridColumn, 0));
-
-    // Base case scenario so it the algorithm
-    // doesn't go in an infinite loop.
     dynamicProgramming[0][0] = 1;
 
     for (int i = 0; i < gridRow; ++i)
@@ -95,12 +87,10 @@ int dynamicProgrammingAlgorithm(
             {
                 if (i > 0)
                 {
-                    // Above path
                     dynamicProgramming[i][j] += dynamicProgramming[i - 1][j];
                 }
                 if (j > 0)
                 {
-                    // Left path
                     dynamicProgramming[i][j] += dynamicProgramming[i][j - 1];
                 }
             }
@@ -110,7 +100,7 @@ int dynamicProgrammingAlgorithm(
     return dynamicProgramming[gridRow - 1][gridColumn - 1];
 }
 
-// Main to test
+// Main function
 int main()
 {
     vector<vector<char>> grid = {
@@ -123,8 +113,54 @@ int main()
         {'.', '.', '.', '.', '.', 'X', '.', 'X'},
         {'.', '.', '.', '.', '.', '.', '.', '.'}};
 
-    cout << "Algorithm 1: " << exhaustiveSearchAlgorithm(grid) << endl;
-    cout << "Algorithm 2: " << dynamicProgrammingAlgorithm(grid) << endl;
+    // Open file to write execution times in the plots folder
+    ofstream outFile("execution.txt");
+    outFile << "# Grid Size | Exhaustive Search Time (microseconds) | Dynamic Programming Time (microseconds)\n";
+
+    // Get the actual grid size
+    int maxGridSize = grid.size();
+
+    // Measure execution time for Exhaustive Search and Dynamic Programming
+    for (int size = 5; size <= maxGridSize; size += 5)
+    {
+        // Debugging message for subgrid size
+        cout << "Processing subgrid of size " << size << "x" << size << endl;
+
+        // Ensure that the subgrid size does not exceed the grid size
+        if (size > maxGridSize)
+        {
+            cout << "Error: Requested subgrid size exceeds grid size!" << endl;
+            break; // Exit the loop when the size exceeds the grid's max size
+        }
+
+        // Create subgrid of the current size
+        vector<vector<char>> subGrid(size, vector<char>(size));
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                subGrid[i][j] = grid[i][j];
+            }
+        }
+
+        // Measure execution time for Exhaustive Search
+        auto start = high_resolution_clock::now();
+        exhaustiveSearchAlgorithm(subGrid);
+        auto stop = high_resolution_clock::now();
+        auto duration1 = duration_cast<microseconds>(stop - start);
+
+        // Measure execution time for Dynamic Programming
+        start = high_resolution_clock::now();
+        dynamicProgrammingAlgorithm(subGrid);
+        stop = high_resolution_clock::now();
+        auto duration2 = duration_cast<microseconds>(stop - start);
+
+        // Write execution times to the plots/execution.txt file
+        outFile << size << " " << duration1.count() << " " << duration2.count() << "\n";
+    }
+
+    outFile.close();
+    cout << "Execution times written to 'execution.txt'" << endl;
 
     return 0;
 }
